7cb7cca127973ee31bdbde438c3677400c145d9e
Matheus Lopes <164070452+Mathlps@users.noreply.github.com>
2024-05-24 21:10:30 -0300

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,27 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-
-	int nums[10], i;
 	
-	int soma=0;
+	char v1[10], v2[10], aux[10];
 	
-	for(i=0; i<10; i++){
-		printf("Digite o numero %d:", i);
-		scanf("%d", &nums[i]);
- 		printf("%d\n", nums[i]);
+	
+	fgets(v1, sizeof(v1), stdin);
+    fgets(v2, sizeof(v2), stdin);
+	
+	if(strcmp(v1,v2) > 0){
+		strcpy(aux, v1);
+		strcpy(v1, v2);
+		strcpy(v2, aux);
 	}
-	
-	for(i=0; i<10; i++){
-		
-		soma=soma+nums[i];
-	}
-	
-	float media=soma/10;
-	
-	printf("media=%.2f", media);
- 
-	
+	printf("v1= %s e \n v2= %s", v1, v2);
 	
 	return 0;
 }
